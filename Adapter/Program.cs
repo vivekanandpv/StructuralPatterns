36326d5006596bf979ee1c9550971eebb7dd1010
Vivekanand P V <vivekanandpv@outlook.com>
2021-12-15 10:17:47 +0530

    adapter and bridge

---
+++ Adapter/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace Adapter {
+    class Program {
+        static void Main(string[] args)
+        {
+            IK8sService minikube = new Minikube();
+            IK8sService eksVariant1 = new ObjectAdapter(new ElasticK8sService());
+            IK8sService eksVariant2 = new ClassAdapter();
+
+            minikube.Start();
+            minikube.Stop();
+
+            eksVariant1.Start();
+            eksVariant1.Stop();
+
+            eksVariant2.Start();
+            eksVariant2.Stop();
+        }
+    }
+
+    interface IK8sService {
+        void Start();
+        void Stop();
+    }
+
+    class Minikube : IK8sService {
+        public void Start() {
+            Console.WriteLine("Minikube starts");
+        }
+
+        public void Stop() {
+            Console.WriteLine("Minikube stops");
+        }
+    }
+
+    class ElasticK8sService {
+        public virtual void Run() {
+            Console.WriteLine("EKS starts running");
+        }
+
+        public virtual void Halt() {
+            Console.WriteLine("EKS halts");
+        }
+    }
+
+    class ObjectAdapter : IK8sService
+    {
+        private readonly ElasticK8sService _eks;
+
+        public ObjectAdapter(ElasticK8sService eks)
+        {
+            _eks = eks;
+        }
+
+        public void Start()
+        {
+            _eks.Run();
+        }
+
+        public void Stop()
+        {
+            _eks.Halt();
+        }
+    }
+
+    class ClassAdapter: ElasticK8sService, IK8sService
+    {
+        public void Start()
+        {
+            Run();
+        }
+
+        public void Stop()
+        {
+            Halt();
+        }
+
+        public override void Run()
+        {
+            base.Run();
+        }
+
+        public override void Halt()
+        {
+            base.Halt();
+        }
+    }
+}
---
+++ Bridge/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+
+namespace Bridge {
+    class Program {
+        static void Main(string[] args) {
+            IServer awsServer = new AwsEc2();
+            VirtualInfrastructureBase infrastructure = new CloudVirtualInfrastructure(awsServer);
+
+            infrastructure.Instantiate();
+            infrastructure.Stop();
+
+            IServer azureServer = new AzureVm();
+            infrastructure.SetServer(azureServer);
+
+            infrastructure.Instantiate();
+            infrastructure.Stop();
+        }
+    }
+
+    interface IServer {
+        public bool HasStarted { get; set; }
+        void PowerOn();
+        void InstallMachineImage();
+        void SetupUserAccount();
+        void InstallAdditionalSoftware();
+        void StartOperation();
+        void StopOperation();
+        void PowerOff();
+    }
+
+    class AwsEc2 : IServer {
+        public bool HasStarted { get; set; }
+        public void PowerOn() {
+            HasStarted = true;
+            Console.WriteLine("AWS EC2: power on");
+        }
+
+        public void InstallMachineImage() {
+            Console.WriteLine("AWS EC2: installing machine image");
+        }
+
+        public void SetupUserAccount() {
+            Console.WriteLine("AWS EC2: setting up user account");
+        }
+
+        public void InstallAdditionalSoftware() {
+            Console.WriteLine("AWS EC2: installing additional software");
+        }
+
+        public void StartOperation() {
+            Console.WriteLine("AWS EC2: starting server operation");
+        }
+
+        public void StopOperation() {
+            Console.WriteLine("AWS EC2: stopping server operation");
+        }
+
+        public void PowerOff() {
+            HasStarted = false;
+            Console.WriteLine("AWS EC2: power off");
+        }
+    }
+
+    class AzureVm : IServer {
+        public bool HasStarted { get; set; }
+        public void PowerOn() {
+            HasStarted = true;
+            Console.WriteLine("Azure Virtual Machine: power on");
+        }
+
+        public void InstallMachineImage() {
+            Console.WriteLine("Azure Virtual Machine: installing machine image");
+        }
+
+        public void SetupUserAccount() {
+            Console.WriteLine("Azure Virtual Machine: setting up user account");
+        }
+
+        public void InstallAdditionalSoftware() {
+            Console.WriteLine("Azure Virtual Machine: installing additional software");
+        }
+
+        public void StartOperation() {
+            Console.WriteLine("Azure Virtual Machine: starting server operation");
+        }
+
+        public void StopOperation() {
+            Console.WriteLine("Azure Virtual Machine: stopping server operation");
+        }
+
+        public void PowerOff() {
+            HasStarted = false;
+            Console.WriteLine("Azure Virtual Machine: power off");
+        }
+    }
+
+    abstract class VirtualInfrastructureBase {
+        protected IServer Server { get; set; }
+
+        protected VirtualInfrastructureBase(IServer server) {
+            this.Server = server;
+        }
+
+        public abstract void Instantiate();
+
+        public abstract void Stop();
+
+        public abstract void SetServer(IServer server);
+    }
+
+    class CloudVirtualInfrastructure : VirtualInfrastructureBase {
+        public CloudVirtualInfrastructure(IServer server) : base(server) {
+        }
+
+        public override void SetServer(IServer server) {
+            Server = server;
+        }
+
+        public override void Instantiate() {
+            Server.PowerOn();
+            Server.InstallMachineImage();
+            Server.InstallAdditionalSoftware();
+            Server.SetupUserAccount();
+            Server.StartOperation();
+        }
+
+        public override void Stop() {
+            if (Server.HasStarted) {
+                Server.StopOperation();
+                Server.PowerOff();
+            }
+        }
+    }
+}
